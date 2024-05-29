e3904fdb082a467ac98cfe0c415c466918e8898e
Astaper <astapowitsch@gmail.com>
2024-05-29 23:30:21 +0300

    added tabs

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,5 +1,39 @@
-const tabs = () => {
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector);
+    const tab = document.querySelectorAll(tabSelector);
+    const contents = document.querySelectorAll(contentSelector);
 
+    const hideTabContent = () => {
+        contents.forEach(content => {
+            content.style.display = 'none';
+        });
+
+        tab.forEach(tab => {
+            tab.classList.remove(activeClass);
+        });
+    }
+
+    const showTabContent = (i = 0) => {
+        contents[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) ||
+                target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((tab, i) => {
+                if (target == tab || target.parentNode == tab) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 };
 
 export default tabs;
