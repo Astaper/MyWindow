ed2a83351eb11030f37676e957fa05fafdee135b
Astaper <astapowitsch@gmail.com>
2024-06-01 23:06:21 +0300

    added fetch request

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,5 +1,70 @@
 const forms = () => {
+    const form = document.querySelectorAll('form');
+    const inputs = document.querySelectorAll('input');
+    const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
 
+    phoneInputs.forEach(input => {
+        input.addEventListener('input', () => {
+            input.value = input.value.replace(/\D/, '');
+        });
+    });
+
+    const message = {
+        loading: 'Загрузка...',
+        succes: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+            //body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(input => {
+            input.value = '';
+        });
+    };
+
+    form.forEach(form => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            form.appendChild(statusMessage);
+
+            const formData = new FormData(form);
+            const jsonData = {};
+            formData.forEach((value, key) => {
+                jsonData[key] = value;
+            });
+
+            console.log(jsonData);
+
+            postData('https://local-qpbb.onrender.com/api/data', jsonData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.succes;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
 };
 
 export default forms;
